b383f0a85049fe3a5ec26b0e736e31e2194bffd7
CheezItMan <chris@adadevelopersacademy.org>
2020-06-24 15:42:45 -0700

    remove files

--- custom-snippets/pacific-nw/submission.rb
+++
@@ -1,9 +0,0 @@
-print "Enter a State ==> "
-state = gets.chomp
-
-if state == "Idaho" || state == "Washington" || state == "Oregon"
-  puts "This state is in the PNW"
-else
-  puts "You should move to the PNW.  It's great here!"
-end
-
